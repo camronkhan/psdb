680edf99c1953dd0d9d8fa6bee26ab3cd415ddf2
Camron Khan <camron.khan@gmail.com>
2016-03-17 12:12:20 -0500

    added application helper for sorting of product search results

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+	def sortable(column_title = nil)
+		direction = column_title == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
+		link_to(column_title, params.merge(:sort => column_title, :direction => direction, :page => nil))
+	end
+
 end

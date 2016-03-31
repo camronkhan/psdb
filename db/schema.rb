9ccf7b0ae5b246eb36a34f83c0f39580f34c9591
Camron Khan <camron.khan@gmail.com>
2016-03-31 16:27:09 -0500

    reset db

--- db/schema.rb
+++ db/schema.rb
@@ -11,75 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160317220345) do
+ActiveRecord::Schema.define(version: 20160331182226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pg_trgm"
-  enable_extension "fuzzystrmatch"
-
-  create_table "companies", force: :cascade do |t|
-    t.string   "name"
-    t.string   "website_url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "companies", ["name"], name: "index_companies_on_name", unique: true, using: :btree
-
-  create_table "products", force: :cascade do |t|
-    t.integer  "company_id"
-    t.string   "name"
-    t.string   "image_url"
-    t.string   "tags"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "products", ["company_id"], name: "index_products_on_company_id", using: :btree
-  add_index "products", ["name", "company_id"], name: "index_products_on_name_and_company_id", unique: true, using: :btree
-
-  create_table "servicer_assignments", force: :cascade do |t|
-    t.integer  "product_id"
-    t.integer  "servicer_id"
-    t.string   "condition"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "servicer_assignments", ["product_id", "servicer_id"], name: "servicer_assignments_index", using: :btree
-
-  create_table "servicers", force: :cascade do |t|
-    t.string   "name"
-    t.string   "internal_phone"
-    t.string   "external_phone"
-    t.string   "address"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
-  add_index "servicers", ["name"], name: "index_servicers_on_name", unique: true, using: :btree
-
-  create_table "technologist_assignments", force: :cascade do |t|
-    t.integer  "product_id"
-    t.integer  "technologist_id"
-    t.string   "condition"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  add_index "technologist_assignments", ["product_id", "technologist_id"], name: "technologist_assignments_index", using: :btree
-
-  create_table "technologists", force: :cascade do |t|
-    t.string   "name"
-    t.string   "internal_phone"
-    t.string   "external_phone"
-    t.string   "hours"
-    t.string   "escalation"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
-  add_index "technologists", ["name"], name: "index_technologists_on_name", unique: true, using: :btree
 
 end

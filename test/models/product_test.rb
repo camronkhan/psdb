88e747bd21d950ba01716bad123787821c4040e7
Camron Khan <camron.khan@gmail.com>
2016-03-31 10:36:04 -0500

    added name presence test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,11 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not create product without a name" do
+  	product = Product.new
+  	assert_not product.save
+  end
+
+  
 end

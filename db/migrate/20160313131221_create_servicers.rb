fbe73897f2551a3956580dc1b24698accf6c6309
Camron Khan <camron.khan@gmail.com>
2016-03-31 09:27:14 -0500

    added unique index on servicer name

--- db/migrate/20160313131221_create_servicers.rb
+++ db/migrate/20160313131221_create_servicers.rb
@@ -7,5 +7,6 @@ class CreateServicers < ActiveRecord::Migration
       t.string :address
       t.timestamps null: false
     end
+    add_index :servicers, :name, unique: true	# unique index on servicer name
   end
 end

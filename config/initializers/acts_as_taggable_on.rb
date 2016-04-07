a4e3da430c48f28d8ed3879b5c4b43933c886064
Camron Khan <camron.khan@gmail.com>
2016-04-07 10:33:02 -0500

    set initializer t: 1) remove nonreferenced tags, 2) downcase tags

---
+++ config/initializers/acts_as_taggable_on.rb
@@ -0,0 +1,8 @@
+# Remove unused tag objects after removing taggings
+ActsAsTaggableOn.remove_unused_tags = true
+
+# Force tags to be saved downcased
+ActsAsTaggableOn.force_lowercase = true
+
+# Change the tag list delimiter
+#ActsAsTaggableOn.delimiter = ' ' # Example: Use space as delimiter

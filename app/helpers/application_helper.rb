5bde7e0d040ead5ddf1bf190b6198e31c2d46adc
Camron Khan <camron.khan@gmail.com>
2016-04-29 11:21:57 -0500

    removed response code check for images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,11 +6,4 @@ module ApplicationHelper
 		link_to(column_title, params.merge(:sort => direction, :page => nil))
 	end
 
-	# Checks if the image URI is valid, returns placeholder image if not
-	def validateImage(image_url)
-		require 'open-uri' 
-		open(image_url) do |f|
-			f.status.first
-		end
-	end
 end

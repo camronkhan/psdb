946e6882f00be62e56a195d34bbf65a94364db7e
Camron Khan <camron.khan@gmail.com>
2016-05-14 08:29:27 -0500

    removed turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require jquery-ui/autocomplete
 //= require autocomplete-rails
-//= require turbolinks
 //= require bootstrap-sprockets
 //= require cocoon
 //= require_tree .

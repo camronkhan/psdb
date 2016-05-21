6f0fb28320d5eb9837721972480ee8206df9b27b
Camron Khan <camron.khan@gmail.com>
2016-05-21 11:56:27 -0500

    added persistance for search criteria buttons

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -14,6 +14,20 @@ module ProductsHelper
 			end
 	end
 
+	# Persists active / inactive state for each search criteria button (checkbox)
+	def is_checked?(checkboxname, state)
+	case checkboxname
+			when 'filter_by_product'
+				if state=='on' then 'checked' end
+			when 'filter_by_company'
+				if state=='on' then 'checked' end
+			when 'filter_by_tag'
+				if state=='on' then 'checked' end
+			else
+				return nil
+			end
+	end
+
 	# Format relevance ranking float as percentage
 	def relevance_to_percentage(relevance)
 		relevance *= 100
